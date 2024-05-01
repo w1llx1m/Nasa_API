ff678a7664528d0f2e466014dbd60176fb9410dd
w1llx1m <williamsilva.monteiro28@gmail.com>
2024-05-01 16:01:02 -0300

    creating the main pages

--- NasaImg/views.py
+++ NasaImg/views.py
@@ -1,2 +1,23 @@
 from django.shortcuts import render
+import requests as rq
+import json
 
+def home_view(request):
+    context = {}
+    return render(request, "Main.html", context)
+
+def image_view(request):
+    context = {}
+    return render(request, "Main.html", context)
+
+def image_day_view(request):
+    context = Get_Img_nasa()
+    print(context)
+    return render(request, "imgday.html", context)
+
+def Get_Img_nasa():
+    ret = rq.get("https://api.nasa.gov/planetary/apod?api_key=DVejQ1l5jOwFbFJhafaTQbInVB8rp6inonHtn4mN",)
+    jsonRet = json.loads(ret.content.decode('utf-8'))
+    #for x in jsonRet:
+       #print(jsonRet[x])
+    return jsonRet
--- Nasa_API/urls.py
+++ Nasa_API/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from NasaImg.views import home_view, image_day_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('images-day/', image_day_view),
+
 ]
